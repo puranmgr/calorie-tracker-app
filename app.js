7d80b02bbb8fff089c19a0c5c1fad17f0da6f11e
puranmgr <puranmgr@gmail.com>
2019-01-26 20:43:35 +0000

    added 'insert item to the ui' code

--- app.js
+++ app.js
@@ -95,6 +95,20 @@ const UICtrl = (function() {
 				calories: document.querySelector(UISelectors.itemCaloriesInput).value
 			};
 		},
+
+		addListItem: (item) => {
+			// Create li element
+			const li = document.createElement('li');
+			// Add class
+			li.className = 'collection-item';
+			// Add ID
+			li.id = `item-${item.id}`;
+			// Add HTML
+			li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>`;
+			// Insert item
+			document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+		},
 		getSelectors: () => {
 			return UISelectors;
 		}
@@ -121,6 +135,8 @@ const App = (function(ItemCtrl, UICtrl) {
 		if (input.name !== '' && input.calories !== '') {
 			// Add item
 			const newItem = ItemCtrl.addItem(input.name, input.calories);
+			// Add item to UI list
+			UICtrl.addListItem(newItem);
 		}
 		e.preventDefault();
 	};
