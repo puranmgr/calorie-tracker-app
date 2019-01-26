bbb3c5940814a31d62c8b8781e7fbbe7f68b2901
puranmgr <puranmgr@gmail.com>
2019-01-26 13:05:56 +0000

    get & populate items

--- app.js
+++ app.js
@@ -1,81 +1,84 @@
 // Storage Controller
 
-
-
-
 // Item Controller
-const ItemCtrl = (function () {
-    // Item Constructor
-    const Item = function (id, name, calories) {
-        this.id = id;
-        this.name = name;
-        this.calories = calories;
-    }
+const ItemCtrl = (function() {
+	// Item Constructor
+	const Item = function(id, name, calories) {
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+	};
 
-    // Data Structure / State
-    const data = {
-        items: [
+	// Data Structure / State
+	const data = {
+		items: [
+			{
+				id: 0,
+				name: 'Bread with Jam',
+				calories: 270
+			},
 
-            {
-                id: 0,
-                name: 'Bread with Jam',
-                calories: 270
-            },
+			{
+				id: 1,
+				name: 'Meatball Pasta',
+				calories: 800
+			},
 
-            {
-                id: 1,
-                name: 'Meatball Pasta',
-                calories: 800
-            },
+			{
+				id: 2,
+				name: 'Chocolate Protein shake',
+				calories: 100
+			}
+		],
 
-            {
-                id: 2,
-                name: 'Chocolate Protein shake',
-                calories: 100
-            }
-        ],
+		currentItem: null,
+		totalCalories: 0
+	};
 
-        currentItem: null,
-        totalCalories: 0
-    }
-
-    // Public methods
-    return {
-        logData: () => {
-            return data;
-        }
-    }
-
-
+	// Public methods
+	return {
+		getItems: () => {
+			return data.items;
+		},
+		logData: () => {
+			return data;
+		}
+	};
 })();
-
-
 
 // UI Controller
-const UICtrl = (function () {
+const UICtrl = (function() {
+	// Public method
+	return {
+		populateItemList: (items) => {
+			let html = '';
 
-    // Public method
-    return {
+			items.forEach((item) => {
+				html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+            </li>`;
+			});
 
-    }
-
+			// Insert list items
+			document.querySelector('#item-list').innerHTML = html;
+		}
+	};
 })();
 
-
-
 // App Controller
-const App = (function (ItemCtrl, UICtrl) {
+const App = (function(ItemCtrl, UICtrl) {
+	// Public methods
+	return {
+		init: () => {
+			// Fetch items from data structure
+			const items = ItemCtrl.getItems();
 
-    // Public methods
-    return {
-        init: () => {
-            console.log('Initializing App...');
-        }
-    }
-
+			// Populate list with items
+			UICtrl.populateItemList(items);
+		}
+	};
 })(ItemCtrl, UICtrl);
-
-
 
 // Initialize App
 App.init();
