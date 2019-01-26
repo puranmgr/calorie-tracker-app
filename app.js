7ffed96d48f2257fb883368e8bc363424437b223
puranmgr <puranmgr@gmail.com>
2019-01-26 14:13:56 +0000

    get form input from UI Controller

--- app.js
+++ app.js
@@ -50,7 +50,9 @@ const ItemCtrl = (function() {
 const UICtrl = (function() {
 	const UISelectors = {
 		itemList: '#item-list',
-		addBtn: '.add-btn'
+		addBtn: '.add-btn',
+		itemNameInput: '#item-name',
+		itemCaloriesInput: '#item-calories'
 	};
 	// Public method
 	return {
@@ -67,7 +69,12 @@ const UICtrl = (function() {
 			// Insert list items
 			document.querySelector(UISelectors.itemList).innerHTML = html;
 		},
-
+		getItemInput: () => {
+			return {
+				name: document.querySelector(UISelectors.itemNameInput).value,
+				calories: document.querySelector(UISelectors.itemCaloriesInput).value
+			};
+		},
 		getSelectors: () => {
 			return UISelectors;
 		}
@@ -87,7 +94,10 @@ const App = (function(ItemCtrl, UICtrl) {
 
 	// Add item submit
 	const itemAddSubmit = (e) => {
-		console.log('Add');
+		// Get form input from UI Controller
+		const input = UICtrl.getItemInput();
+
+		console.log(input);
 		e.preventDefault();
 	};
 	// Public methods
