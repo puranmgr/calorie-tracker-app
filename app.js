593a735554a3f12467127180a0f3b5ef7231c63b
puranmgr <puranmgr@gmail.com>
2019-01-29 19:58:10 +0000

    click edit item

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ const App = (function(ItemCtrl, UICtrl) {
 		document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
 		// Edit icon click event
-		document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+		document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
 	};
 
 	// Add item submit
@@ -214,8 +214,8 @@ const App = (function(ItemCtrl, UICtrl) {
 		e.preventDefault();
 	};
 
-	// Update item submit
-	const itemUpdateSubmit = (e) => {
+	// Click edit item
+	const itemEditClick = (e) => {
 		if (e.target.classList.contains('edit-item')) {
 			// Get list item id (item-0, item-1)
 			const listId = e.target.parentNode.parentNode.id;
