736fe396abed2141d8650c95d30b4398d918b615
puranmgr <puranmgr@gmail.com>
2019-01-31 17:40:19 +0000

    clear the state and update calories

--- app.js
+++ app.js
@@ -334,6 +334,14 @@ const App = (function(ItemCtrl, UICtrl) {
 		// Delete from UI
 		UICtrl.deleteListItem(currentItem.id);
 
+		// Get total calories
+		const totalCalories = ItemCtrl.getTotalCalories();
+
+		// Add total calories to UI
+		UICtrl.showTotalCalories(totalCalories);
+
+		UICtrl.clearEditState();
+
 		e.preventDefault();
 	};
 	// Public methods
